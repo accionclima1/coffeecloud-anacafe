47e38c7d7cbaa91e083110a4fbcca8f58acee5a3
DiegoArgueta <dargueta1879@gmail.com>
2018-10-22 22:49:14 -0600

    Se corrigió problema con la fecha de Roya

--- routes/index.js
+++ routes/index.js
@@ -1445,6 +1445,7 @@ router.post('/roya', auth, function (req, res, next) {
     roya.severidadPromedio = req.body.avgplntDmgPct;
     roya.idunidad = req.body.idunidad;
     roya.loteIndex = req.body.loteIndex;
+    roya.createdAt=req.body.date;
 
     roya.save(function (err, roya) {
         if (err) { return next(err); }
@@ -2000,35 +2001,35 @@ if (enfermedad=="Broca") {
           var dayStart;
           var monthStart=monthNow;
           var yearStart=yearNow;
-    
-    
+
+
          if (dayNow>7) {
-    
+
           var dayStart=dayNow-7;
-    
+
          } else {
-    
+
           var dayStart=26;
           monthStart--;
-    
-         }
-    
-         var dateStart=new Date(yearNow,monthStart,dayStart);
-    
-    
-    
 
-    
-    
+         }
+
+         var dateStart=new Date(yearNow,monthStart,dayStart);
+
+
+
+
+
+
     /*
-    
+
           console.log("Dia: "+dayStart+" Month: "+monthStart+" Year: "+yearStart);
           console.log("Dia: "+dayNow+" Month: "+monthNow+" Year: "+yearNow);
           return res.json("Dia: "+dayNow+" Month: "+monthNow+" Year: "+yearNow);*/
-    
-    
-    
-    
+
+
+
+
     });
 
 
