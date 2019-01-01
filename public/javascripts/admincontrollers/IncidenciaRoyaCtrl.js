5ce124db8f6c754421ea23985372a2a60a67e16f
DiegoArgueta <dargueta1879@gmail.com>
2019-01-01 03:42:43 -0600

    Se cambió el punto de referencia del mapa de enfermedad.

--- public/javascripts/admincontrollers/IncidenciaRoyaCtrl.js
+++ public/javascripts/admincontrollers/IncidenciaRoyaCtrl.js
@@ -170,7 +170,7 @@ for (var i = 0; i < $scope.deptos.length; i++) {
 	$scope.mostrarMapa = function(){
 
 	if(mymap==null){
-		mymap = L.map('royamap').setView([14.973642, -90.450439], 8);
+		mymap = L.map('royamap').setView([13.734155, -88.785374], 8);
 	}else{
 
 	}
