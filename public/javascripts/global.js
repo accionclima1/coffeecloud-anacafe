59cf859044071c1569af337e47ea6783e26afbf0
DiegoArgueta <dargueta1879@gmail.com>
2019-01-01 04:39:59 -0600

    Se cambio el DNS por IP + Port

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,7 +1,7 @@
 ﻿var global = {};
 global.setting = {
-    //service: "http://coffeecloud.centroclima.org/",
-    service: "http://coffeecloud.centroclima.org/",
+    //service: "http://163.172.138.232:8090/",
+    service: "http://163.172.138.232:8090/",
     getServiceUrl: function () {
         return this.service;
     }
