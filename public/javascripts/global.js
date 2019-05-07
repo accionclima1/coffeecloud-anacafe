d6de75395456245c3c8ff6d47f46840d18ea69f3
ChaozzJ <direccion@epicalesoft.com>
2019-05-07 11:58:10 -0600

    redirect to anacafe.coffeecloudapp.com

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,7 +1,7 @@
 ﻿var global = {};
 global.setting = {
     //service: "http://coffeecloud.centroclima.org/",
-    service: "https://coffeecloud.centroclima.org/",
+    service: "http://anacafe.coffeecloudapp.com/",
     getServiceUrl: function () {
         return this.service;
     }
